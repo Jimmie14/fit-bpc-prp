59f10ab5230bf1988a05527b8d4aab4540210886
Samuel Poul <dev@overefactor.com>
2026-02-24 16:18:06 +0000

    chore: add devcontainer

---
+++ .devcontainer/ros_env.sh
@@ -0,0 +1,11 @@
+# shellcheck disable=SC1090
+
+# Source ROS underlay
+if [ -n "${ROS_DISTRO:-}" ] && [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
+  source "/opt/ros/${ROS_DISTRO}/setup.bash"
+fi
+
+# Source overlay if built
+if [ -f "$HOME/ros2_ws/install/setup.bash" ]; then
+  source "$HOME/ros2_ws/install/setup.bash"
+fi
