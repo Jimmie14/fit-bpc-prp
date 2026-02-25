603583bc9458e205eb24918343ced141ca9e9527
Samuel Poul <dev@overefactor.com>
2026-02-25 19:31:38 +0000

    chore: final devcontainer fixes

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -2,9 +2,6 @@
 # shellcheck disable=SC1090,SC1091
 set -euo pipefail
 
-echo "Hello world!"
-echo "test" >> "$HOME/ros2_ws/entrypoint.log"
-
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "$HOME/ros2_ws/install/setup.bash"
