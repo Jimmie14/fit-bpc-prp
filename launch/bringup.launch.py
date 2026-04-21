149629f238f332f254d225647f1bdfb8aa132727
Samuel Poul <dev@overefactor.com>
2026-04-21 14:15:45 +0000

    feat: add ImuComponent republishing imu with covariance and correct frame

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -3,29 +3,31 @@ from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
 
     launch_manhattan = LaunchConfiguration('launch_manhattan', default='true')
 
-    ekf_config = os.path.join(
-        os.getenv('AMENT_PREFIX_PATH').split(':')[0],
-        'share/project/config/ekf.yaml'
-    )
+    project_share_dir = get_package_share_directory('project')
+    ekf_config = os.path.join(project_share_dir, 'config', 'ekf.yaml')
+    print(f"ekf conf: {ekf_config}")
 
     nodes = [
         DeclareLaunchArgument(
             'launch_manhattan',
             default_value='true',
             description='Launch the Manhattan node'
-        ), Node(
+        ),
+        Node(
             package='robot_localization',
             executable='ekf_node',
             name='ekf_filter_node',
             output='screen',
             parameters=[ekf_config]
-        ), Node(
+        ),
+        Node(
             package='project',
             executable='manhattan',
             name='manhattan',
---
+++ src/Controllers/ImuComponent.cpp
@@ -0,0 +1,58 @@
+
+#include "ImuComponent.hpp"
+
+using namespace std;
+
+constexpr auto baseImuTopic = "/bpc_prp_robot/imu";
+constexpr auto imuTopic = "~/imu";
+constexpr auto imuAngularCovariance = 0.01;
+constexpr auto imuLinearCovariance = 0.01;
+
+namespace Manhattan::Core {
+
+ImuComponent::ImuComponent(const App& app)
+    : BaseController(app)
+{
+
+}
+
+void ImuComponent::OnEnable()
+{
+    _imuSubscriber = _node->create_subscription<sensor_msgs::msg::Imu>(
+        baseImuTopic, 2,
+        [this](const sensor_msgs::msg::Imu::SharedPtr msg) { this->OnImu(msg); }
+    );
+
+    _imuPublisher = _node->create_publisher<sensor_msgs::msg::Imu>(imuTopic, 2);
+
+    RCLCPP_INFO(_node->get_logger(), "ImuComponent enabled");
+}
+
+void ImuComponent::OnDisable()
+{
+    _imuSubscriber.reset();
+    _imuPublisher.reset();
+}
+
+void ImuComponent::OnImu(const sensor_msgs::msg::Imu::SharedPtr& msg) const
+{
+    auto resultMsg = *msg;
+
+    resultMsg.header.frame_id = "base_link";
+
+    resultMsg.angular_velocity_covariance = {
+        imuAngularCovariance, 0.0, 0.0,
+        0.0, imuAngularCovariance, 0.0,
+        0.0, 0.0, imuAngularCovariance
+    };
+
+    resultMsg.linear_acceleration_covariance = {
+        imuAngularCovariance, 0.0, 0.0,
+        0.0, imuAngularCovariance, 0.0,
+        0.0, 0.0, imuAngularCovariance
+    };
+
+    _imuPublisher->publish(resultMsg);
+}
+
+}
